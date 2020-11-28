a6db12fc239988c385b017add44a376336e4e568
Manoj Ramakrishnan <manoj.rgv@gmail.com>
2020-11-29 00:33:56 +0530

    Spring boot app with external tomcat configuration complete code

---
+++ springboot-tomcat/src/main/java/com/javaevangelist/tomcat/api/SpringbootTomcatApplication.java
@@ -0,0 +1,19 @@
+package com.javaevangelist.tomcat.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringbootTomcatApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTomcatApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringbootTomcatApplication.class);
+    }
+}
